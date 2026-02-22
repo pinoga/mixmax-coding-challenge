d74a8f85a33b292357804514becfe1f4f71aff69
Fernando Bizzotto <fbizzotto@blackpointcyber.com>
2026-02-22 17:47:26 +0000

    fix: make date validation regexp stateless

--- src/validators/date-validator.ts
+++ src/validators/date-validator.ts
@@ -4,7 +4,6 @@ import zod, { ZodString } from "zod";
 export class DateValidator {
   public static "YYYY-MM-DDThhRegExp" = new RegExp(
     "^\\d{4}-\\d{2}-\\d{2}T\\d{2}$",
-    "g",
   );
 
   public static "YYYY-MM-DDThhRegExpDate"(z: typeof zod): ZodString {
